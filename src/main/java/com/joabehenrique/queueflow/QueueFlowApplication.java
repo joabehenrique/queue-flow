b14bda64d0b31983d6fa73f7dc225ee9196d3694
joabehenrique <henriquecidoz@hotmail.com>
2023-03-15 20:33:29 -0300

    initial commit

---
+++ src/main/java/com/joabehenrique/queueflow/QueueFlowApplication.java
@@ -0,0 +1,13 @@
+package com.joabehenrique.queueflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joabehenrique/queueflow/QueueFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joabehenrique.queueflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
