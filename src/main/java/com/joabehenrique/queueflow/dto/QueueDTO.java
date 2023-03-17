e61e3a587b99edf07878c4216e6457702f8e9c02
joabehenrique <henriquecidoz@hotmail.com>
2023-03-17 17:21:19 -0300

    feat: created entity and dto queue

---
+++ src/main/java/com/joabehenrique/queueflow/dto/QueueDTO.java
@@ -0,0 +1,11 @@
+package com.joabehenrique.queueflow.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class QueueDTO {
+    private String field1;
+    private Integer field2;
+}
---
+++ src/main/java/com/joabehenrique/queueflow/entity/Queue.java
@@ -0,0 +1,13 @@
+package com.joabehenrique.queueflow.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Queue {
+    private String field1;
+    private Integer field2;
+}
