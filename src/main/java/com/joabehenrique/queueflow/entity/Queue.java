0a09ab7e94de2a64e539e2b521e7fb773ec2b4f4
joabehenrique <henriquecidoz@hotmail.com>
2023-03-18 23:55:55 -0300

    feat: added json property for jackson lib

--- src/main/java/com/joabehenrique/queueflow/entity/Queue.java
+++ src/main/java/com/joabehenrique/queueflow/entity/Queue.java
@@ -1,13 +1,18 @@
 package com.joabehenrique.queueflow.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
 
+@Data
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Queue {
+
+    @JsonProperty("field1")
     private String field1;
+
+    @JsonProperty("field2")
     private Integer field2;
 }
