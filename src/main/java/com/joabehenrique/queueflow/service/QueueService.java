f8b9b4b33f79ccdcb30b1d1230c47af3653a4aea
joabehenrique <henriquecidoz@hotmail.com>
2023-03-18 22:56:42 -0300

    feat: added new value in queue

--- src/main/java/com/joabehenrique/queueflow/service/QueueService.java
+++ src/main/java/com/joabehenrique/queueflow/service/QueueService.java
@@ -1,6 +1,7 @@
 package com.joabehenrique.queueflow.service;
 
 
+import com.joabehenrique.queueflow.dto.QueueDTO;
 import com.joabehenrique.queueflow.entity.Queue;
 import com.joabehenrique.queueflow.messaging.MessageSender;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +12,10 @@ public class QueueService {
     @Autowired
     private MessageSender messageSender;
 
-    public void processMyModel(Queue myModel) {
+    public Queue processMyModel(QueueDTO queue) {
+        Queue queueNew = new Queue("ei", 1);
         String a = "aaa";
         messageSender.sendMessage(a);
+        return queueNew;
     }
 }
