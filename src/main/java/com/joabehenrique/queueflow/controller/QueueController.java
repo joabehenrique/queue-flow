64784dbcbe3f82e783e5d74425a07a26f34f652e
joabehenrique <henriquecidoz@hotmail.com>
2023-03-18 22:55:00 -0300

    feat: updated endpoint name

--- src/main/java/com/joabehenrique/queueflow/controller/QueueController.java
+++ src/main/java/com/joabehenrique/queueflow/controller/QueueController.java
@@ -1,20 +1,27 @@
 package com.joabehenrique.queueflow.controller;
 
+import com.joabehenrique.queueflow.dto.QueueDTO;
 import com.joabehenrique.queueflow.entity.Queue;
 import com.joabehenrique.queueflow.service.QueueService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping(value = "api/v1/queue")
 public class QueueController {
     @Autowired
     private QueueService myService;
 
-    @PostMapping("/myEndpoint")
-    public void myEndpoint(@RequestBody Queue myModel) {
-        myService.processMyModel(myModel);
+    @PostMapping
+    public ResponseEntity<Queue> myEndpoint(@RequestBody QueueDTO queueBody) {
+        Queue queue = myService.processMyModel(queueBody);
+        return new ResponseEntity<>(queue, HttpStatus.CREATED);
     }
 }
 
