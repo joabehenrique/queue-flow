6d4c104025253be20752ac2654445d54dcee3a11
joabehenrique <henriquecidoz@hotmail.com>
2023-03-18 22:55:45 -0300

    feat: added lombok constructor

--- src/main/java/com/joabehenrique/queueflow/dto/QueueDTO.java
+++ src/main/java/com/joabehenrique/queueflow/dto/QueueDTO.java
@@ -1,10 +1,14 @@
 package com.joabehenrique.queueflow.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class QueueDTO {
     private String field1;
     private Integer field2;
