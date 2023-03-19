b3f9f273088f4a267817fe24433dd90a1bf65b87
joabehenrique <henriquecidoz@hotmail.com>
2023-03-18 23:54:55 -0300

    feat: updated type messages

--- src/main/java/com/joabehenrique/queueflow/messaging/MessageReceiver.java
+++ src/main/java/com/joabehenrique/queueflow/messaging/MessageReceiver.java
@@ -1,14 +1,16 @@
 package com.joabehenrique.queueflow.messaging;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@EnableRabbit
 public class MessageReceiver {
 
     @RabbitListener(queues = "queue-first")
     public void receiveMessage(String message) {
-        System.out.println("Received message: " + message);
+        System.out.println("\nReceived message: " + message + "\n");
     }
 }
 
--- src/main/java/com/joabehenrique/queueflow/messaging/MessageSender.java
+++ src/main/java/com/joabehenrique/queueflow/messaging/MessageSender.java
@@ -1,5 +1,8 @@
 package com.joabehenrique.queueflow.messaging;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.joabehenrique.queueflow.dto.QueueDTO;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +17,9 @@ public class MessageSender {
     @Autowired
     private Queue myQueue;
 
-    public void sendMessage(String message) {
+    public void sendMessage(QueueDTO message) throws JsonProcessingException {
+        String json = new ObjectMapper().writeValueAsString(message);
+        rabbitTemplate.convertAndSend("queue-first", json);
         rabbitTemplate.convertAndSend(myQueue.getName(), message);
     }
 }
