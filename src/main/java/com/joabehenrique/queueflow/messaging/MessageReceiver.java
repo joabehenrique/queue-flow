3c20cfb154baabf619116c64e7337380bab28a98
joabehenrique <henriquecidoz@hotmail.com>
2023-03-21 22:47:55 -0300

    feat: updated receive rabbit and added kafka receive

--- src/main/java/com/joabehenrique/queueflow/messaging/MessageReceiver.java
+++ src/main/java/com/joabehenrique/queueflow/messaging/MessageReceiver.java
@@ -1,7 +1,11 @@
 package com.joabehenrique.queueflow.messaging;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.joabehenrique.queueflow.dto.QueueDTO;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,8 +13,14 @@ import org.springframework.stereotype.Component;
 public class MessageReceiver {
 
     @RabbitListener(queues = "queue-first")
-    public void receiveMessage(String message) {
-        System.out.println("\nReceived message: " + message + "\n");
+    public void receiveMessageRabbitMQ(String message) throws JsonProcessingException {
+        QueueDTO queueDTO = new ObjectMapper().readValue(message, QueueDTO.class);
+        System.out.println("\nReceived message RabbitMQ: " + queueDTO + "\n");
+    }
+
+    @KafkaListener(topics = "my-topic", groupId = "my-group")
+    public void receiveMessageKafka(String message) {
+        System.out.println("Received message Kafka: " + message);
     }
 }
 
